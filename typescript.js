4fc57fce5aed4ea2771a3a96f592547c186d0594
Heejae <chris@kcd.co.kr>
2021-03-25 17:32:55 +0900

    Update typescript.js
    Co-authored-by: Hwidong Bae <bae.hwidong@gmail.com>

--- typescript.js
+++ typescript.js
@@ -20,7 +20,7 @@ module.exports = {
         // Type 만 사용하는 경우 import 문에 type을 명시적으로 붙여줍니다. (https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-8.html#type-only-imports-and-export)
         '@typescript-eslint/consistent-type-imports': 'error',
 
-        // return type은 명시적으로 적지 않고 추론을 이용합니다. (compilerOptions.noImplicitAny 사용을 권장합니다.)
+        // 함수의 return type은 명시적으로 적는 대신 타입 추론을 이용합니다. (tsconfig에서 compilerOptions.noImplicitAny 사용을 권장합니다.)
         '@typescript-eslint/explicit-module-boundary-types': 'off',
       },
     },
