f1526817dbf982a41ab107c0833de1dbf162c53a
Chris <chris@kcd.co.kr>
2021-03-25 15:30:45 +0900

    Update react.js

--- react.js
+++ react.js
@@ -13,6 +13,7 @@ module.exports = {
     'react/jsx-filename-extension': ['error', {
       extensions: ['.jsx', '.tsx'],
     }],
+
     // import 문을 통일하고, React 내장 Hooks는 `React.useState()` 형태로 사용합니다.
     // Bad: `import React, { useState } from 'react';`
     // Bad: `import * as React from 'react';`
@@ -34,6 +35,7 @@ module.exports = {
         ],
       }],
     }],
+
     // funcion 키워드는 named function으로 사용합니다.
     // unnamed component는 대부분 callback이나 함수의 return 으로 사용되기 때문에 arrow function으로 사용합니다.
     'react/function-component-definition': ['error', {
