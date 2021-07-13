7b06aaf56276b368fd652a5fe2a246b33c6fd422
Chris <chris@kcd.co.kr>
2021-07-13 22:26:44 +0900

    `react-jsx` 사용으로 불필요해진 react import 룰 제거

--- react.js
+++ react.js
@@ -36,28 +36,6 @@ module.exports = {
       },
     ],
 
-    // import 문을 통일하고, React 내장 Hooks는 `React.useState()` 형태로 사용합니다.
-    // Bad: `import React, { useState } from 'react';`
-    // Bad: `import * as React from 'react';`
-    // Good: `import React from 'react';`
-    'no-restricted-imports': ['error', {
-      paths: [{
-        name: 'react',
-        importNames: [
-          'useState',
-          'useEffect',
-          'useContext',
-          'useReducer',
-          'useCallback',
-          'useMemo',
-          'useRef',
-          'useImperativeHandle',
-          'useLayoutEffect',
-          'useDebugValue',
-        ],
-      }],
-    }],
-
     // React component는 funcion 키워드를 사용하는 named function으로 선언합니다.
     // 콜백이나 함수의 반환 값으로 사용되는 unnamed component는 arrow function으로 작성합니다.
     'react/function-component-definition': ['error', {
