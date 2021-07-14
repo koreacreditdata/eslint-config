573077cfd10882fecfc06073955f8be27bd68ddc
Heejae <chris@kcd.co.kr>
2021-07-14 13:24:17 +0900

    Strict rules (#14)

--- react.js
+++ react.js
@@ -13,12 +13,6 @@ module.exports = {
     // 3항연산자 중첩 허용: 적절하게 쓰면 가독성을 해치지 않음
     'no-nested-ternary': 'off',
 
-    // 한글 컴포넌트 import를 위함
-    'react/jsx-pascal-case': 'off',
-
-    // 한글/영어 혼용된 컴포넌트/메서드명 사용을 위함
-    'camelcase': 'off',
-
     // typescript를 무조건 쓸 것을 가정하므로 defaultProps 정의할 필요 없음
     'react/require-default-props': 'off',
 
@@ -30,10 +24,7 @@ module.exports = {
       extensions: ['.jsx', '.tsx'],
     }],
 
-    // div에 키보드 이벤트 없이 onClick 자유롭게 넣을 수 있게 함
-    'jsx-a11y/click-events-have-key-events': 'off',
-
-    // label 태그는 control 관련 태그를 감싸고 있어야 함
+    // label 태그는 control 관련 태그를 감싸고 있어야 함 (assert: 'both'를 'either'로 변경)
     'jsx-a11y/label-has-associated-control': [
       'error',
       {
@@ -44,28 +35,6 @@ module.exports = {
         depth: 25,
       },
     ],
-
-    // import 문을 통일하고, React 내장 Hooks는 `React.useState()` 형태로 사용합니다.
-    // Bad: `import React, { useState } from 'react';`
-    // Bad: `import * as React from 'react';`
-    // Good: `import React from 'react';`
-    'no-restricted-imports': ['error', {
-      paths: [{
-        name: 'react',
-        importNames: [
-          'useState',
-          'useEffect',
-          'useContext',
-          'useReducer',
-          'useCallback',
-          'useMemo',
-          'useRef',
-          'useImperativeHandle',
-          'useLayoutEffect',
-          'useDebugValue',
-        ],
-      }],
-    }],
 
     // React component는 funcion 키워드를 사용하는 named function으로 선언합니다.
     // 콜백이나 함수의 반환 값으로 사용되는 unnamed component는 arrow function으로 작성합니다.
